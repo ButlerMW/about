9d9a3823b7561747ba040ae9b46f02b8fc3e3404
Michael Butler <michael_w_butler@live.com>
2021-02-04 13:56:42 -0700

    first commit to remastered page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 🚧 This page is currently under contruction 🚧 </h1>
+      <p class="bio">
+          Michael Butler always had a sense for adventure and learning. From a young age he enjoyed outdoor 
+          activities and developed interests for natural sciences which later developed into all forms of 
+          science and mathematics.  
+      </p>
+      <p class="bio">
+          After Michael graduated high school, he attended the University of Idaho where he continued to 
+          study various sciences such as geology, geography, mathematics, physics, soil sciences, chemistry 
+          and biology. During this time, Michael started to develop another passion - learning new softwares. 
+      </p>
+      <p class="bio">
+          After graduating, Michael obtained a position as a survey crewman for one of Idaho’s best engineering firms. 
+          Although he did not have any experience and did not know much about land surveying, Michael worked hard and 
+          learned as much as he could and within less than three years became a Crew Chief and worked on both large and 
+          small projects around the state of Idaho. 
+          During his time as a land surveyor, Michael fell in love with the technologies and softwares, so much so that 
+          he made the decision to move from land surveying and pursue the passion of software. Michael left his job and 
+          attended Coding Dojo, Boise branch to learn as much as he could about coding languages, software, and web development.
+
+      </p>
+      <p class="bio">
+          Michael graduated from Coding Dojo with flying colors and high praise from his fellow classmates and instructor. 
+          Out of all the various things Michael has learned, one thing is clear; Michael can learn new things quickly and 
+          efficiently. His dream now is to continue to grow and strengthen upon the experiences and skills learned to become 
+          a member of the technology world. 
+      </p>
     </div>
   );
 }
